13e576cc56a6355a2161bd9ac89875024f0cb92b
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2020-09-24 13:17:29 -0600

    Debugging 1

--- Program.cs
+++ Program.cs
@@ -15,28 +15,30 @@ namespace Fibunacci.PerezDamas
                 switch(option)
                 {
                     case 1:
+                        int option2 = 0;
                         do{
                             Console.Clear();
                             Console.WriteLine("How many times do u want algoritm to repeat? ");
-                            int rep = Convert.ToInt32(Console.ReadLine());
+                            int rep = Convert.ToInt32(Console.ReadLine()) + 1;
                             Console.WriteLine(" \n \nEnter to Watch ur result...");
                             Console.ReadKey();
                             Console.WriteLine($"El Ultimo Resultado de tu Algoritmo es: {suma(0,1,rep)}");
                             Console.ReadKey();
                             do{
+                                Console.Clear();
                                 Console.WriteLine(" \nWhat do u wanna do now? \n \n1.Create another algoritm \n2.Back to the mother menú");
-                                option = Convert.ToInt32(Console.ReadLine());
-                                if(option != 1 && option != 2)
+                                option2 = Convert.ToInt32(Console.ReadLine());
+                                if(option2 != 1 && option2 != 2)
                                     Console.WriteLine("The selected option does not exist, ENTER and try again...");
-                            }while(option != 1 && option != 2);
-                        }while(option != 2);                    
+                            }while(option2 != 1 && option2 != 2);
+                        }while(option2 != 2);                    
                         break;
                     case 2:
                         Console.WriteLine("Thanks for ussing my program! I guees it was usefully for u! ;)");
                         Console.ReadKey();
                         break;
                     default:
-                        Console.WriteLine("The selected option does not exist, ENTER and try again...");
+                        Console.WriteLine(" \nThe selected option does not exist, ENTER and try again...");
                         Console.ReadKey();
                         break;
                 }
@@ -44,18 +46,20 @@ namespace Fibunacci.PerezDamas
         }
         private static int suma(int a, int b, int c)
         {
-            if(c==0){
+            if(c<1){
                 Console.WriteLine($"{c} ");
                 return c;
             }
             else{
                 c --;
-                int n = a;
-                int m = b;
+                if(c == 0){
+                    int n = a;
+                    int m = b;
 
-                a = m;
-                b = n+m;
-
+                    a = m;
+                    b = n+m;
+                }
+                Console.WriteLine($"Valor de C = {c}, Valor de a = {a}, Valor de b = {b}");
                 int r = b + suma(a,b,c);
                 Console.WriteLine($"{r} ");
                 return r;
