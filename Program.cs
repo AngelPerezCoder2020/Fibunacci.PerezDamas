0424b81c45c7795cb05b26ed96b08574cb8336b9
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2020-09-24 13:04:33 -0600

    I added a Do while in the main menu

--- Program.cs
+++ Program.cs
@@ -7,38 +7,40 @@ namespace Fibunacci.PerezDamas
         static void Main(string[] args)
         {
             int option = 0;
-            Console.Clear();
-            Console.WriteLine("Welcome to my Program \n \nWhat do u wanna do? \n \n1. Create a Fibonacci´s Algoritm \n2.EXIT");
-            option = Convert.ToInt32(Console.ReadLine());
+            do{
+                Console.Clear();
+                Console.WriteLine("Welcome to my Program \n \nWhat do u wanna do? \n \n1. Create a Fibonacci´s Algoritm \n2. EXIT");
+                option = Convert.ToInt32(Console.ReadLine());
 
-            switch(option)
-            {
-                case 1:
-                    do{
-                        Console.Clear();
-                        Console.WriteLine("How many times do u want algoritm to repeat? ");
-                        int rep = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine(" \n \nEnter to Watch ur result...");
-                        Console.ReadKey();
-                        Console.WriteLine($"El Ultimo Resultado de tu Algoritmo es: {suma(0,1,rep)}");
-                        Console.ReadKey();
+                switch(option)
+                {
+                    case 1:
                         do{
-                            Console.WriteLine(" \nWhat do u wanna do now? \n \n1.Create another algoritm \n2.Back to the mother menú");
-                            option = Convert.ToInt32(Console.ReadLine());
-                            if(option != 1 && option != 2)
-                                Console.WriteLine("The selected option does not exist, ENTER and try again...");
-                        }while(option != 1 && option != 2);
-                    }while(option != 2);                    
-                    break;
-                case 2:
-                    Console.WriteLine("Thanks for ussing my program! I guees it was usefully for u! ;)");
-                    Console.ReadKey();
-                    break;
-                default:
-                    Console.WriteLine("The selected option does not exist, ENTER and try again...");
-                    Console.ReadKey();
-                    break;
-            }
+                            Console.Clear();
+                            Console.WriteLine("How many times do u want algoritm to repeat? ");
+                            int rep = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(" \n \nEnter to Watch ur result...");
+                            Console.ReadKey();
+                            Console.WriteLine($"El Ultimo Resultado de tu Algoritmo es: {suma(0,1,rep)}");
+                            Console.ReadKey();
+                            do{
+                                Console.WriteLine(" \nWhat do u wanna do now? \n \n1.Create another algoritm \n2.Back to the mother menú");
+                                option = Convert.ToInt32(Console.ReadLine());
+                                if(option != 1 && option != 2)
+                                    Console.WriteLine("The selected option does not exist, ENTER and try again...");
+                            }while(option != 1 && option != 2);
+                        }while(option != 2);                    
+                        break;
+                    case 2:
+                        Console.WriteLine("Thanks for ussing my program! I guees it was usefully for u! ;)");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("The selected option does not exist, ENTER and try again...");
+                        Console.ReadKey();
+                        break;
+                }
+            }while(option != 2);
         }
         private static int suma(int a, int b, int c)
         {
